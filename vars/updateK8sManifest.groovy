ed84f27f8e2443dd11a0913796b58e54056575b1
Mostafa Mahmoud <145359101+MostafaMahmoud2020@users.noreply.github.com>
2025-08-15 17:30:08 +0300

    Update updateK8sManifest.groovy

--- vars/updateK8sManifest.groovy
+++ vars/updateK8sManifest.groovy
@@ -1,9 +1,9 @@
 def call() {
     withCredentials([string(credentialsId: 'github-token', variable: 'GIT_TOKEN')]) {
         sh """
-            rm -rf argocd-test
-            git clone https://$GIT_TOKEN@github.com/MostafaMahmoud2020/argocd-test.git
-            cd argocd-test
+            rm -rf ${WORKSPACE}/argocd-test
+            git clone https://${GIT_TOKEN}@github.com/MostafaMahmoud2020/argocd-test.git ${WORKSPACE}/argocd-test
+            cd ${WORKSPACE}/argocd-test
             sed -i 's|image: .*|image: mostafamahmoud0/test-java-app:v${env.BUILD_NUMBER}|' deployment.yaml
             git config user.email "jenkins@example.com"
             git config user.name "Jenkins"
